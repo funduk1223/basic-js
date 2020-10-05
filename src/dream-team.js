e1681dd10b315eba205784021eac35c8d6dffccb
Mikhail Akhmedzyanov <mikhail.akhmedzyanov@gmail.com>
2020-10-05 14:11:42 +0300

    feat: done dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let teamName = [];
+  let checkMembers = true;
+  if (Array.isArray(members)){
+    members.forEach(function(unit){
+      if (typeof unit === 'string'){
+        let str = unit.replace(/\s+/g,'');
+        teamName.push(str[0].toUpperCase());
+      }
+    });
+    
+    let result = teamName.sort().join('');
+  
+    return (checkMembers === true ? result : false);
+  }
+  else return false;
 };
