dab608353b39edde0d08f9034a2c061d4ccc931b
Mikhail Akhmedzyanov <mikhail.akhmedzyanov@gmail.com>
2020-10-07 01:01:28 +0300

    feat: done transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  let newArray = arr;
+  let map = new Map();
+  let disNext = '--discard-next';
+  let disPrev = '--discard-prev';
+  let dobNext = '--double-next';
+  let dobPrev = '--double-prev';
+
+  if (arr === []) return [];
+  if (!Array.isArray(arr)) throw new Error()
+  else {
+
+    for(let i=0; i<=newArray.length-1; i++){
+      map.set(i, newArray[i])
+    } 
+  }
+
+  let result = [];
+  map.forEach((value, key, map)=>{
+    if (value === disNext){
+   
+      map.set(key, undefined);
+      map.set(key+1, undefined);
+      console.log(map);
+    }
+    if (value === disPrev ){
+      
+      map.set(key, undefined);
+      map.set(key-1, undefined);
+
+    }
+    if (value === dobNext){
+     
+      map.set(key, map.get(key+1, value));
+
+    }
+    if (value === dobPrev){
+
+      map.set(key, map.get(key-1, value));
+
+    }
+  });
+
+  for (let obj of map.values()){
+    if (obj !== undefined){
+      result.push(obj);
+    }
+  }
+
+  return result;
+}
