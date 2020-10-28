19188e62a2ff668333e61bf5f7a2487903baefe6
Mikhail Akhmedzyanov <mikhail.akhmedzyanov@gmail.com>
2020-10-28 20:00:30 +0300

    feat: add solve for simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  len: 0,
+  chainArray: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let lenChain = 0;
+    for (chain in chainMaker) {
+      if (chain.slice(0, 5) === 'chain') {
+        if (chainMaker[chain] !== 'deleted'){
+          lenChain++;
+        }
+      }
+    }
+    return lenChain;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined){
+      this.len++;
+      this.chainArray.push(`( )`)
+    }
+    else {
+      this.len++;
+      this.chainArray.push(`( ${value} )`);
+    }
+    return chainMaker;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let pos = parseInt(position, 10) - 1;
+    if (pos < 0 || pos > this.chainArray.length || !Number.isInteger(pos)){
+      this.chainArray = [];
+      throw new Error("Неправильная позиция!");
+    }
+    else this.chainArray.splice(pos, 1); 
+    return chainMaker;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chainArray.reverse();
+    return chainMaker;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let resChain = '';
+    this.chainArray.forEach((item, index, arr) => {
+      if (index !== arr.length-1) {
+        resChain += `${item}~~`;
+      } else resChain += `${item}`; 
+    });
+    this.chainArray = [];
+    return resChain;
   }
+
 };
 
 module.exports = chainMaker;
